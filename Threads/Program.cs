84428e924eaf31fe1d51f698056b53b693bed139
Andrew Popko <popkoandr@gmail.com>
2016-09-20 17:42:06 +0300

    first sample

---
+++ Threads/Program.cs
@@ -0,0 +1,33 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+ using System.Threading;
+ using System.Threading.Tasks;
+
+namespace Threads
+{
+    class Program
+    {
+        // method that we are going to execute on another thread / (simple)
+        private static void WriteSecond()
+        {
+            while (true)
+            {
+                Console.WriteLine(new string(' ', 10) + "Secondary");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ThreadStart writeSecondDelegate = new ThreadStart(WriteSecond);
+            Thread thread = new Thread(writeSecondDelegate);
+            thread.Start();
+
+            while (true)
+            {
+                Console.WriteLine("Primary");
+            }
+        }
+    }
+}
